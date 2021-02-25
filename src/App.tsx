26f9c3b76cfc1108b208ee33b1e53f831820ffc1
timo <tim.mohr@exxeta.com>
2021-02-25 22:23:20 +0100

    Project init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+
+const App:React.FC = () => {
+  useEffect(() => {
+    axios.get('/api/hello')
+        .then(res => setState(res.data))
+  }, [])
+
+  const [state, setState] = useState<any>('')
+  return(
+      <div>
+        <p>{state.response_text}</p>
+      </div>
+  )
+};
+export default App;
